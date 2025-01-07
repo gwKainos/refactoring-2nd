ebe247a3862a97011cb3eafae311ec164720634e
Kainos <gwkainos@gmail.com>
2025-01-07 20:28:14 +0900

    Refactor: 코드 리팩토링 - play 변수 제거

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,6 @@
 import plays from "./plays.json";
 import invoices from "./invoices.json";
 
-function amountFor(aPerformance: { audience: number; }, play: { type: any; }) {
-  let thisAmount = 0;
-  switch (play.type) {
-    case "tragedy":   // 비극
-      thisAmount = 40000;
-      if (aPerformance.audience > 30) {
-        thisAmount += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-
-    case "comedy":   // 희극
-      thisAmount = 30000;
-      if (aPerformance.audience > 20) {
-        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      thisAmount += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
-  }
-  return thisAmount;
-}
-
 export default function index({invoice, plays}: { invoice: any, plays: any }) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -34,21 +11,45 @@ export default function index({invoice, plays}: { invoice: any, plays: any }) {
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+  function playFor(aPerformance: { audience?: number; playID?: any; }) {
+    return plays[aPerformance.playID];
+  }
 
+  function amountFor(aPerformance: { audience: number; }) {
+    let thisAmount = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":   // 비극
+        thisAmount = 40000;
+        if (aPerformance.audience > 30) {
+          thisAmount += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+
+      case "comedy":   // 희극
+        thisAmount = 30000;
+        if (aPerformance.audience > 20) {
+          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        thisAmount += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return thisAmount;
+  }
+
+  for (let perf of invoice.performances) {
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} (${
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
         perf.audience
     }석)\n`;
-    totalAmount += thisAmount;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
