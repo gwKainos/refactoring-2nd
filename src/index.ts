1e0d4876cf2aee45add5946648460a402813a78a
Kainos <gwkainos@gmail.com>
2025-01-07 23:06:52 +0900

    Refactor: 코드 리팩토링 - 함수 선언 바꾸기 - format -> usd

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ export default function index({invoice, plays}: { invoice: any, plays: any }) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+
+  function usd(aNumber: number) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
 
   function playFor(aPerformance: { audience?: number; playID?: any; }) {
     return plays[aPerformance.playID];
@@ -54,13 +57,13 @@ export default function index({invoice, plays}: { invoice: any, plays: any }) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
         perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 }
