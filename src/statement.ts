9b30b6b698399c801435cb1a7a89f436d7119831
Kainos <gwkainos@gmail.com>
2025-01-08 12:55:58 +0900

    Refactor: 코드 리팩토링 - 함수명 변경

--- src/statement.ts
+++ src/statement.ts
@@ -8,12 +8,12 @@ export function statement(invoice: Invoice, plays: Plays): string {
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
-  function format(aNumber: number): string {
+  function usd(aNumber: number): string {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber)
+    }).format(aNumber / 100)
   }
 
   function volumeCreditsFor(aPerformance: Performance): number {
@@ -31,13 +31,13 @@ export function statement(invoice: Invoice, plays: Plays): string {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
         perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
