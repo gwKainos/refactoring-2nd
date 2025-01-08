2b69142200317c01c99d69cebaa0ea74d86ca202
Kainos <gwkainos@gmail.com>
2025-01-08 15:33:12 +0900

    Refactor: 코드 리팩토링 - 파일 분리

---
+++ src/createStatementData.ts
@@ -0,0 +1,95 @@
+import plays from "./plays.json";
+import { EnrichedPerformance, Invoice, Performance, StatementData } from "./types/invoiceTypes";
+import { Play, Plays } from "./types/playTypes";
+
+export function createStatementData(invoice: Invoice, plays: Plays) {
+  const statementData = {
+    customer: invoice.customer,
+    performances: invoice.performances.map(enrichPerformance),
+    totalAmount: 0,
+    totalVolumeCredits: 0
+  }
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData)
+  return renderPlainText(statementData, plays);
+}
+
+function enrichPerformance(aPerformance: Performance): EnrichedPerformance {
+  const result: any = Object.assign({}, aPerformance);
+  result.play = playFor(result);
+  result.amount = amountFor(result);
+  result.volumeCredits = volumeCreditsFor(result);
+
+  return result
+}
+
+function renderPlainText(data: StatementData, plays: Plays) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += `${perf.play.name}: ${usd(perf.amount)} (${
+        perf.audience
+    }석)\n`;
+  }
+
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function amountFor(aPerformance: EnrichedPerformance) {
+  let result = 0;
+
+  switch (aPerformance.play.type) {
+    case "tragedy":   // 비극
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+
+    case "comedy":   // 희극
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+  }
+  return result;
+}
+
+function playFor(aPerformance: Performance): Play {
+  // @ts-ignore
+  return plays[aPerformance.playID];
+}
+
+function usd(aNumber: number): string {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100)
+}
+
+function volumeCreditsFor(aPerformance: EnrichedPerformance): number {
+  let result = 0;
+
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === aPerformance.play.type) {
+    result += Math.floor(aPerformance.audience / 5);
+  }
+
+  return result;
+}
+
+function totalVolumeCredits(data: StatementData): number {
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+}
+
+function totalAmount(data: StatementData): number {
+  return data.performances.reduce((total, p) => total + p.amount, 0);
+}
