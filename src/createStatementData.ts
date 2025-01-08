8017311a65e8e2d4928177d53087f9b91a5dae35
Kainos <gwkainos@gmail.com>
2025-01-08 17:53:56 +0900

    Refactor: 코드 리팩토링 - 공연료 계산기 만들기 - amountFor -> class로 이동

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -23,38 +23,39 @@ class createPerformanceCalculator implements PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
+
+  get amount(): number {
+    let result = 0;
+
+    switch (this.play.type) {
+      case "tragedy":   // 비극
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+
+      case "comedy":   // 희극
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
 }
 
 function enrichPerformance(aPerformance: Performance): EnrichedPerformance {
   const calculator = new createPerformanceCalculator(aPerformance, playFor(aPerformance))
   const result: any = Object.assign({}, aPerformance);
   result.play = playFor(result);
-  result.amount = amountFor(result);
+  result.amount = calculator.amount;
   result.volumeCredits = volumeCreditsFor(result);
-  return result;
-}
-
-function amountFor(aPerformance: EnrichedPerformance) {
-  let result = 0;
-
-  switch (aPerformance.play.type) {
-    case "tragedy":   // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-
-    case "comedy":   // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-  }
   return result;
 }
 
