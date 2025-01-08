2a2738a40a8bc2b4543a7a7b85834f08b1db760f
Kainos <gwkainos@gmail.com>
2025-01-08 18:51:23 +0900

    Refactor: 코드 리팩토링 - 서브 클래스 처리

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -3,31 +3,6 @@ import { EnrichedPerformance, Invoice, Performance, StatementData } from "./type
 import { Play, Plays } from "./types/playTypes";
 import { IPerformanceCalculator } from "./types/interface";
 
-class TragedyCalculator {
-  constructor(aPerformance: Performance, aPlay: Play) {
-    
-  }
-
-}
-
-class ComedyCalculator {
-  constructor(aPerformance: Performance, aPlay: Play) {
-    
-  }
-
-}
-
-const createPerformanceCalculator = (aPerformance: Performance, aPlay: Play) => {
-  switch (aPlay.type) {
-    case 'tragedy':
-      return new TragedyCalculator(aPerformance, aPlay)
-    case 'comedy':
-      return new ComedyCalculator(aPerformance, aPlay)
-    default:
-      return new PerformanceCalculator(aPerformance, aPlay)
-  }
-}
-
 class PerformanceCalculator implements IPerformanceCalculator {
   performance: Performance;
   play: Play;
@@ -38,39 +13,42 @@ class PerformanceCalculator implements IPerformanceCalculator {
   }
 
   get amount(): number {
-    let result = 0;
-
-    switch (this.play.type) {
-      case "tragedy":   // 비극
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-
-      case "comedy":   // 희극
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-    return result;
+    throw new Error('서브 클래스에서 처리하도록 설계되었습니다.')
   }
 
   get volumeCredits(): number {
-    let result = 0;
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
 
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) {
-      result += Math.floor(this.performance.audience / 5);
-    }
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000
+    if (this.performance.audience > 30) result += 1000 * (this.performance.audience - 30)
+    return result
+  }
+}
 
-    return result;
+class ComedyCalculator extends PerformanceCalculator {
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+  get amount() {
+    let result = 30000
+    if (this.performance.audience > 20) result += 10000 + 500 * (this.performance.audience - 20)
+    result += 300 * this.performance.audience
+    return result
+  }
+}
+
+const createPerformanceCalculator = (aPerformance: Performance, aPlay: Play) => {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay)
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay)
+    default:
+      return new PerformanceCalculator(aPerformance, aPlay)
   }
 }
 
