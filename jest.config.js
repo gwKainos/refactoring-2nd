30c0c512bf7219a87fd79dff56939266cd3512cb
Kainos <gwkainos@gmail.com>
2025-01-09 10:40:20 +0900

    책 환경에 맞춰 변경

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-};
