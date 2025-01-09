aeb8fdc61483aedbbba8346be7dd7cab88a80692
Kainos <gwkainos@gmail.com>
2025-01-09 16:44:15 +0900

    Refactor: 코드 리팩토링 - 배송비 변수 추출 (Command+Option+V)

--- src/refactoring03.js
+++ src/refactoring03.js
@@ -1,10 +1,11 @@
 export function price(order) {
   const basePrice = order.quantity * order.itemPrice;
-  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice
+      * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
   return (
-      // 가격(price) = 기본가격 - 수량할인 + 배송비
-      basePrice -
+       basePrice -
       quantityDiscount +
-      Math.min(basePrice * 0.1, 100)
+      shipping
   )
 }
