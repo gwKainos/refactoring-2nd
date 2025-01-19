c88301b7720f4045853ce37502415cb182351e2f
Kainos <gwkainos@gmail.com>
2025-01-19 20:23:32 +0900

    Refactor: 코드 리팩토링 - 매개변수 추가하기 - 테스트 코드에서도 매개변수 추가

--- test/refactoring05-02.test.js
+++ test/refactoring05-02.test.js
@@ -5,17 +5,17 @@ import {inNewEngland} from "../src/refactoring05-02.js";
 describe("inNewEngland", () => {
   it("returns true for a customer in New England", () => {
     const customer = { address: { state: "MA" } };
-    expect(inNewEngland(customer)).to.be.true;
+    expect(inNewEngland(customer.address.state)).to.be.true;
   });
 
   it("returns false for a customer outside New England", () => {
     const customer = { address: { state: "NY" } };
-    expect(inNewEngland(customer)).to.be.false;
+    expect(inNewEngland(customer.address.state)).to.be.false;
   });
 
   it("returns false for a customer with no state", () => {
     const customer = { address: {} };
-    expect(inNewEngland(customer)).to.be.false;
+    expect(inNewEngland(customer.address.state)).to.be.false;
   });
 
   it("returns false for a customer with no address", () => {
@@ -23,7 +23,7 @@ describe("inNewEngland", () => {
       name: 'Paul',
       address: { state: 'AA' },
     };
-    expect(inNewEngland(customer)).to.be.false;
+    expect(inNewEngland(customer.address.state)).to.be.false;
   });
 });
 
@@ -36,7 +36,7 @@ describe("New England customers filter", () => {
       { name: "Dave", address: { state: "CA" } },
     ];
 
-    const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+    const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
 
     expect(newEnglanders).to.have.lengthOf(2);
     expect(newEnglanders).to.deep.equal([
@@ -51,7 +51,7 @@ describe("New England customers filter", () => {
       { name: "Bob", address: { state: "CA" } },
     ];
 
-    const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+    const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
 
     expect(newEnglanders).to.have.lengthOf(0);
     expect(newEnglanders).to.deep.equal([]);
@@ -59,7 +59,7 @@ describe("New England customers filter", () => {
 
   it("handles an empty customers list", () => {
     const someCustomers = [];
-    const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+    const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
     expect(newEnglanders).to.have.lengthOf(0);
     expect(newEnglanders).to.deep.equal([]);
   });
