3d9f0aa724c57188129ce534b87800ab22107bc7
Kainos <gwkainos@gmail.com>
2025-01-19 19:20:27 +0900

    Refactor: 코드 리팩토링 - 매개변수 추가하기 - 새로운 메서드 생성

--- src/refactoring05.js
+++ src/refactoring05.js
@@ -4,7 +4,11 @@ class Book {
   }
 
   addReservation(customer) {
-    this._reservations.push({ customer });
+    this.zz_addReservation(customer);
+  }
+
+  zz_addReservation(customer) {
+    this._reservations.push({customer});
   }
 
   _isDuplicateReservation(customer) {
