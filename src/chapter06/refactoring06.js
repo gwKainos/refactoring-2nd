d1104fe4ef6961864f254cebdbe7850c67f7ad52
Kainos <gwkainos@gmail.com>
2025-01-23 23:26:34 +0900

    Refactor: 코드 리팩토링 - get, set 생성

--- src/chapter06/refactoring06.js
+++ src/chapter06/refactoring06.js
@@ -1,1 +1,9 @@
 let defaultOwner = {firstName: '마틴', lastName: '파울러'};
+
+export function getDefaultOwner() {
+  return defaultOwner;
+}
+
+export function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
