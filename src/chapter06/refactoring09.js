5935dd065775dd6a6bfbcdda33ef6d0ba40b75d8
Kainos <gwkainos@gmail.com>
2025-01-24 13:22:35 +0900

    Refactor: 코드 리팩토링 - 레코드를 클래스로 변환하기 위해 레코드를 캡슐화

--- src/chapter06/refactoring09.js
+++ src/chapter06/refactoring09.js
@@ -7,6 +7,27 @@ function acquireReading() {
   };
 }
 
+class Reading {
+  constructor(data) {
+    this._customer = data.customer
+    this._quantity = data.quantity
+    this._month = data.month
+    this._year = data.year
+  }
+  get customer() {
+    return this._customer
+  }
+  get quantity() {
+    return this._quantity
+  }
+  get month() {
+    return this._month
+  }
+  get year() {
+    return this._year
+  }
+}
+
 function baseRate(month, year) {
   if (year === 2017) {
     return 0.05;
