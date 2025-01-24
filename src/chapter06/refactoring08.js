629757e5208368a97a5c2d45da1c1376a469ce7e
Kainos <gwkainos@gmail.com>
2025-01-24 09:54:13 +0900

    Refactor: 코드 리팩토링 - 클래스 생성

--- src/chapter06/refactoring08.js
+++ src/chapter06/refactoring08.js
@@ -22,4 +22,18 @@ const alert = readingsOutsideRange(station, operatingPlan.temperatureFloor,
 
 console.info("alert", alert)
 
+
+class NumberRange {
+  constructor(min, max) {
+    this._data = {min: min, max: max};
+  }
+  get min() {
+    return this._data.min;
+  }
+
+  get max() {
+    return this._data.max;
+  }
+}
+
 export {station, readingsOutsideRange};
