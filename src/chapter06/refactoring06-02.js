b374b1ccac0d4cbe30128f42519d2bb6b1f851db
Kainos <gwkainos@gmail.com>
2025-01-24 09:19:01 +0900

    Chapter 06 - 여섯번째-02 샘플 및 테스트 코드 - 레코드 캡슐화 - 변경할수 없게 처리

---
+++ src/chapter06/refactoring06-02.js
@@ -0,0 +1,23 @@
+let defaultOwnerDeta = {firstName: '마틴', lastName: '파울러'};
+
+export function defaultOwner() {
+  return new Person(defaultOwnerDeta);
+}
+export function setDefaultOwner(arg) {
+  defaultOwnerDeta = arg;
+}
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+
+  get lastName() {
+    return this._lastName
+  }
+
+  get firstName() {
+    return this._firstName
+  }
+}
